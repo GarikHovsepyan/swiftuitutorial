58900c7cd1f3c1352f85b9327f3a134a826e4b96
Garik <gar.hovsepian@gmail.com>
2024-01-31 16:28:02 +0400

    init

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Garik Hovsepyan on 31.01.24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
