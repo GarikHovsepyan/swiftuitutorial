b81fdb65881b153f8f4b2eecfbda158888a79133
Garik <gar.hovsepian@gmail.com>
2024-01-31 17:08:57 +0400

    Created MapView, ImageView and combined them

---
+++ Landmarks/CircleeImage.swift
@@ -0,0 +1,23 @@
+//
+//  CirlceImage.swift
+//  Landmarks
+//
+//  Created by Garik Hovsepyan on 31.01.24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
