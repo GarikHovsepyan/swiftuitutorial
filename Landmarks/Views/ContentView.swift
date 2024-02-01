f85f8ad9bf65e0e8232ff43b06c6a071ca83ac24
Garik <gar.hovsepian@gmail.com>
2024-02-01 19:49:09 +0400

    Built lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Garik Hovsepyan on 31.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
